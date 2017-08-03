9cf41a338bf0dca1eb10b1c09bfed2cb4a641ee5
awong36 <little.rice.man@gmail.com>
2017-08-03 08:03:43 -0400

    iperf test sequence with shell commands

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #Project: wifi_test
-#Description: 
+#Description: iperf test sequence for wireless testing
 __author__ = "Adrian Wong"
 import subprocess, time, argparse
 
 
 def main():
     #main starts here
-    parrser = argparse.ArgumentParser(description='iperf test sequence')
-
+    parser = argparse.ArgumentParser(description='iperf test sequence')
+    parser.add_argument(dest='interface', nargs='?', default='wlan0', help='wlan interface (default: wlan0)')
+    
 
 
 if __name__ == "__main__":
