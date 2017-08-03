51484f6baa4dccedaecf3d9055d3f45085d11188
awong36 <little.rice.man@gmail.com>
2017-08-03 14:59:49 -0400

    iperf test sequence with shell commands

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # Project: wifi_test
 # Description: iperf (client) test sequence for wireless testing
 __author__ = "Adrian Wong"
-import os, subprocess, argparse, logging, time
+import os, subprocess, argparse, logging, re, time
+
 
 class myConfig(object):
     # default values, these values can be overwritten by command line args
@@ -16,6 +17,7 @@ class myConfig(object):
     sysPath = '/system/'  # system required storage path
     log = 'example.log'
     filename = linuxPath + logPath + log
+
 
 def argSetup(config):
     parser = argparse.ArgumentParser(description='iperf test sequence')
@@ -32,29 +34,49 @@ def argSetup(config):
 
     return parser
 
+
+def normalize_space(s):
+    """Return s stripped of leading/trailing whitespace
+    and with internal runs of whitespace replaced by a single SPACE"""
+    # This should be a str method :-(
+    return ' '.join(s.split())
+
+
+def reformatMsg(input):
+    output = input.splitlines()
+    removeSpace = [x for x in output if not x.isspace()]
+    result = [normalize_space(i) for i in removeSpace]
+    return result
+
+
 def signalStrength(wifiConfig):
-    sigCmd = subprocess.Popen(['iwconfig', wifiConfig.interface], stdout=subprocess.PIPE)
-    with sigCmd.stdout:
-        for line in iter(sigCmd.stdout.readline, b''):
-            print line,
-        sigCmd.wait()  # wait for the subprocess to exit
+    pattern = re.compile("^\s+|\s*,\s*|\s+$")
+    sigCmd, err = subprocess.Popen(['iwconfig', wifiConfig.interface], stdout=subprocess.PIPE).communicate()
+    test = reformatMsg(sigCmd)
+    print test[2], ", ", test[6]
+
 
 def main():
     # main starts here
     wifiConfig = myConfig()
     parser = argSetup(wifiConfig)
     settings = parser.parse_args()
-    logging.basicConfig(filename=wifiConfig.filename, level=logging.DEBUG)
+    logging.basicConfig(filename=wifiConfig.filename, level=logging.INFO)
 
     signalStrength(wifiConfig)
 
-    cmd = subprocess.Popen(['iperf', '-c', settings.address, '-w', settings.window, '-n', settings.buffer], stdout=subprocess.PIPE)
-    with cmd.stdout:
-        for line in iter(cmd.stdout.readline, b''):
-            print line,
-    cmd.wait()  # wait for the subprocess to exit
+    cmd, err = subprocess.Popen(['iperf', '-c', settings.address, '-w', settings.window, '-n', settings.buffer],
+                                stdout=subprocess.PIPE).communicate()
+    test = reformatMsg(cmd)
+
+    print test[2], ", ", test[4], ", ", test[5]
+    # with cmd.stdout:
+    #     for line in iter(cmd.stdout.readline, b''):
+    #         print line,
+    # cmd.wait()  # wait for the subprocess to exit
 
     signalStrength(wifiConfig)
+
 
 if __name__ == "__main__":
     main()
